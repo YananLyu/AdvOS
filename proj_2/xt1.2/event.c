6f26b7a86d61dc317b9fed508d89f10e44f31134
joycebinu <41137757+joycebinu@users.noreply.github.com>
2019-03-05 20:56:31 -0500

    x1.2

---
+++ proj_2/xt1.2/event.c
@@ -0,0 +1,45 @@
+#include <proc.h>
+
+#define NOT_OCCURRED false
+#define OCCURRED true
+
+// your code will not call xtread_yield() and ctxsw()
+// just use resched()
+
+void xthread_init_ev(struct xthread_event_t * ep) {
+	ep->occurred = NOT_OCCURRED;
+}
+
+void xthread_wait_ev(struct xthread_event_t * ep) {
+
+	int remain = ualarm(0, 0);  // disable time interrupt
+
+	if(ep->occurred == NOT_OCCURRED) {  // if event is not occurred, 
+		ep->Q[qi++] = cid;   // insert/put the current thread into the wait Q
+		isEmpty = false;
+             
+		xtab[cid].xstate = XEVENT;  // change the current thr's state = XEVENT
+		printf("\n ******in wait_ev of {%d} is {%d}\n", ep->Q[qi], xtab[ep->Q[qi]].xstate ); //for testing
+		resched();  // this thr will be back to ready state when someone call it
+		return;
+	} else {
+		ep->occurred = NOT_OCCURRED;
+	}
+
+	ualarm(remain);  // enable timer at ending;
+}
+
+void xthread_set_ev(struct xthread_event_t * ep) {
+	
+	if(isEmpty == false) {  // ep->Q is not empty
+		isEmpty = true;
+			printf("in set_ev of {%d} is {%d}\n", ep->Q[qi], xtab[ep->Q[qi]].xstate ); //for testing
+			ep->occurred = NOT_OCCURRED;
+		while(qi >= 0) {
+			xtab[ep->Q[qi]].xstate = XREADY;
+			ep->Q[qi] = -1;
+		}
+	} else {  // if the Q is empty, the event will be set as OCCURRED
+		ep->occurred = OCCURRED;
+	}
+}
